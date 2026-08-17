a65c107373e474e20fc674f15d95b89ae670ee10
gabristavares <soarestavaresgabriel@gmail.com>
2026-08-17 19:51:51 -0300

    :wq :

---
+++ src/button.tsx
@@ -0,0 +1,11 @@
+export function Button() {
+
+    return (
+        <>
+            <h1>Esse botão pode ser alterado!</h1>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Click me
+            </button>
+        </>
+    )
+}
